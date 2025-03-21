b4fcfd78e0e8aecd07a49eff95f4d5e87351decf
seanj <seanjoelbailey@gmail.com>
2025-03-21 05:10:55 -0700

    19/03/2025 - overhauled user service class

--- src/main/java/domain/ParkingLot.java
+++ src/main/java/domain/ParkingLot.java
@@ -1,4 +1,112 @@
 package domain;
 
+/*
+domain.ParkingLot
+ParkingLot entity class
+Author: Thulani Lunyawo(222828250)
+Date: 17/03/2025
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class ParkingLot {
+
+    private String lotId;
+    private String location;
+    private String openTime;
+    private String closingTime;
+    private double pricePerHour;
+    private Set<ParkingSpot> parkingSpots;
+    private Set<Ticket> tickets;
+
+    // Private constructor to be used by the Builder
+    private ParkingLot(Builder builder) {
+        this.lotId = builder.lotId;
+        this.location = builder.location;
+        this.openTime = builder.openTime;
+        this.closingTime = builder.closingTime;
+        this.pricePerHour = builder.pricePerHour;
+        this.parkingSpots = builder.parkingSpots;
+        this.tickets = builder.tickets;
+    }
+
+    // Getters
+    public String getLotId() { return lotId; }
+    public String getLocation() { return location; }
+    public String getOpenTime() { return openTime; }
+    public String getClosingTime() { return closingTime; }
+    public double getPricePerHour() { return pricePerHour; }
+    public Set<ParkingSpot> getParkingSpots() { return parkingSpots; }
+    public Set<Ticket> getTickets() { return tickets; }
+
+    // Ticket generation logic
+    public Ticket generateTicket(ParkingSpot spot) {
+        Ticket ticket = new Ticket(spot);
+        tickets.add(ticket);
+        return ticket;
+    }
+
+    // ParkingSpot management methods
+    public void addParkingSpot(ParkingSpot spot) {
+        parkingSpots.add(spot);
+    }
+
+    public void removeParkingSpot(ParkingSpot spot) {
+        parkingSpots.remove(spot);
+    }
+
+    public void editParkingSpot(ParkingSpot oldSpot, ParkingSpot newSpot) {
+        removeParkingSpot(oldSpot);
+        addParkingSpot(newSpot);
+    }
+
+    public static class Builder {
+        private String lotId;
+        private String location;
+        private String openTime;
+        private String closingTime;
+        private double pricePerHour;
+        private Set<ParkingSpot> parkingSpots = new HashSet<>();
+        private Set<Ticket> tickets = new HashSet<>();
+
+        public Builder setLotId(String lotId) {
+            this.lotId = lotId;
+            return this;
+        }
+
+        public Builder setLocation(String location) {
+            this.location = location;
+            return this;
+        }
+
+        public Builder setOpenTime(String openTime) {
+            this.openTime = openTime;
+            return this;
+        }
+
+        public Builder setClosingTime(String closingTime) {
+            this.closingTime = closingTime;
+            return this;
+        }
+
+        public Builder setPricePerHour(double pricePerHour) {
+            this.pricePerHour = pricePerHour;
+            return this;
+        }
+
+        public Builder setParkingSpots(Set<ParkingSpot> parkingSpots) {
+            this.parkingSpots = parkingSpots;
+            return this;
+        }
+
+        public Builder setTickets(Set<Ticket> tickets) {
+            this.tickets = tickets;
+            return this;
+        }
+
+        public ParkingLot build() {
+            return new ParkingLot(this);
+        }
+    }
 }
---
+++ src/main/java/repository/ParkingLotRepository.java
@@ -0,0 +1,58 @@
+package repository;
+
+/*
+repository.ParkingLotRepository
+ParkingLot Repository class
+Author: Thulani Lunyuawo(222828250)
+Date: 17/03/2025
+ */
+
+import domain.ParkingLot;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ParkingLotRepository implements IParkingLotRepository {
+
+    private static ParkingLotRepository instance;
+    private Set<ParkingLot> parkingLotDatabase = new HashSet<>();
+
+    // Private constructor to prevent instantiation
+    private ParkingLotRepository() {}
+
+    // Singleton pattern to ensure a single instance
+    public static synchronized ParkingLotRepository getInstance() {
+        if (instance == null) {
+            instance = new ParkingLotRepository();
+        }
+        return instance;
+    }
+
+    @Override
+    public void save(ParkingLot parkingLot) {
+        parkingLotDatabase.add(parkingLot);
+    }
+
+    @Override
+    public ParkingLot findById(String lotId) {
+        return parkingLotDatabase.stream()
+                .filter(parkingLot -> parkingLot.getLotId().equals(lotId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public void delete(String lotId) {
+        parkingLotDatabase.removeIf(parkingLot -> parkingLot.getLotId().equals(lotId));
+    }
+
+    @Override
+    public void update(ParkingLot parkingLot) {
+        delete(parkingLot.getLotId());
+        save(parkingLot);
+    }
+
+    @Override
+    public Set<ParkingLot> findAll() {
+        return parkingLotDatabase;
+    }
+}
