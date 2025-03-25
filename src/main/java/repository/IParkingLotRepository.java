d3287d8f3ac58f8e9b6d7a4c0f2cd2b7c02e0bdc
Thulani Lunyawo <222828250@mycput.ac.za>
2025-03-25 15:16:46 -0700

    24/03/2025 -  ParkingLotRepository, ParkingLotService and IParkingLotRepository updated

--- src/main/java/repository/IParkingLotRepository.java
+++ src/main/java/repository/IParkingLotRepository.java
@@ -1,8 +1,8 @@
 package repository;
 
 /*
-repository.IParkingLotRepository
-ParkingLot Repository Interface
+IParkingLotRepository
+Interface for ParkingLot Repository
 Author: Thulani Lunyawo(222828250)
 Date: 20/03/2025
  */
@@ -10,10 +10,20 @@ Date: 20/03/2025
 import domain.ParkingLot;
 import java.util.Set;
 
-public interface IParkingLotRepository {
-    void save(ParkingLot parkingLot);
-    ParkingLot findById(String lotId);
-    void delete(String lotId);
-    void update(ParkingLot parkingLot);
-    Set<ParkingLot> getAll();
+public interface IParkingLotRepository<T, ID> {
+
+    // Create a new parking lot
+    T create(T parkingLot);
+
+    // Read (find) a parking lot by its ID
+    T read(ID lotId);
+
+    // Update an existing parking lot
+    T update(T parkingLot);
+
+    // Delete a parking lot by its ID
+    boolean delete(ID lotId);
+
+    // Get all parking lots
+    Set<T> getAll();
 }
